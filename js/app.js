2ad349d51774aa36d39f157675f67d8e4825da34
Zahra Alvandi <zalvandi39@gmail.com>
2025-07-09 15:40:02 +0330

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,44 @@
+// const contextmenu = document.querySelector(".contextmenu");
+
+// function showContextMenu(event) {
+//     event.preventDefault();
+//     console.log(contextmenu.style.display);
+
+//     if (contextmenu.style.display === "none") {
+//         contextmenu.style.display = "block";
+//         contextmenu.style.top = `${event.pageY}px`;
+//         contextmenu.style.left = `${event.pageX}px`;
+//     } else {
+//         contextmenu.style.top = `${event.pageY}px`;
+//         contextmenu.style.left = `${event.pageX}px`;
+//     }
+// }
+
+// function hideContextMenu() {
+//     contextmenu.style.display = "none";
+// }
+
+// function hideContextMenuWithEscape(event) {
+//     if (event.key === "Escape") {
+//         contextmenu.style.display = "none";
+//     }
+// }
+
+// document.addEventListener("contextmenu", showContextMenu);
+// document.body.addEventListener("click", hideContextMenu);
+// document.body.addEventListener("keydown", hideContextMenuWithEscape);
+
 const contextmenu = document.querySelector(".contextmenu");
 
 function showContextMenu(event) {
     event.preventDefault();
-    console.log(contextmenu.style.display);
-
     if (contextmenu.style.display === "none") {
         contextmenu.style.display = "block";
-        contextmenu.style.top = `${event.pageY}px`;
         contextmenu.style.left = `${event.pageX}px`;
+        contextmenu.style.top = `${event.pageY}px`;
     } else {
-        contextmenu.style.top = `${event.pageY}px`;
         contextmenu.style.left = `${event.pageX}px`;
+        contextmenu.style.top = `${event.pageY}px`;
     }
 }
 
