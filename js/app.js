6da15247dd8cb8e267bc70864d6262ec3ee19055
Zahra Alvandi <zalvandi39@gmail.com>
2025-07-09 15:29:20 +0330

    part-2

--- js/app.js
+++ js/app.js
@@ -6,8 +6,24 @@ function showContextMenu(event) {
 
     if (contextmenu.style.display === "none") {
         contextmenu.style.display = "block";
-        contextmenu.style.left = 0;
+        contextmenu.style.top = `${event.pageY}px`;
+        contextmenu.style.left = `${event.pageX}px`;
+    } else {
+        contextmenu.style.top = `${event.pageY}px`;
+        contextmenu.style.left = `${event.pageX}px`;
+    }
+}
+
+function hideContextMenu() {
+    contextmenu.style.display = "none";
+}
+
+function hideContextMenuWithEscape(event) {
+    if (event.key === "Escape") {
+        contextmenu.style.display = "none";
     }
 }
 
 document.addEventListener("contextmenu", showContextMenu);
+document.body.addEventListener("click", hideContextMenu);
+document.body.addEventListener("keydown", hideContextMenuWithEscape);
